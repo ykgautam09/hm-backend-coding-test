7dcc187543684bb96418fecfa4c59cebe511d324
ykgautam09 <yateeshgautam1357@gmail.com>
2022-01-03 01:25:53 +0530

    add test script for all endpoints

--- tests/api.test.js
+++ tests/api.test.js
@@ -10,7 +10,7 @@ const buildSchemas = require('../src/schemas');
 
 describe('API tests', () => {
     before((done) => {
-        db.serialize((err) => { 
+        db.serialize((err) => {
             if (err) {
                 return done(err);
             }
@@ -29,4 +29,147 @@ describe('API tests', () => {
                 .expect(200, done);
         });
     });
+
+
+    describe("POST /rides", () => {
+        it("Should return JSON data", (done) => {
+            request(app)
+                .post("/rides")
+                .send({
+                    'start_lat': 1,
+                    'start_long': 2,
+                    'end_lat': 3,
+                    'end_long': 4,
+                    'rider_name': "rider1",
+                    'driver_name': "driver1",
+                    'driver_vehicle': 'vehicle1'
+                })
+                .expect("Content-Type", /json/)
+                .expect(200, done)
+        })
+
+        it("should return VALIDATION_ERROR", (done) => {
+            request(app)
+                .post("/rides")
+                .send({
+                    'start_lat': 199,
+                    'start_long': '2',
+                    'end_lat': '3',
+                    'end_long': '4',
+                    'rider_name': "rider1",
+                    'driver_name': "driver1",
+                    'driver_vehicle': 'vehicle1'
+                })
+                // .end()
+                .expect("Content-Type", /json/)
+                .expect({
+                    error_code: 'VALIDATION_ERROR',
+                    message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+                }, done)
+        })
+
+        it("should return VALIDATION_ERROR", (done) => {
+            request(app)
+                .post("/rides")
+                .send({
+                    'start_lat': 1,
+                    'start_long': 2,
+                    'end_lat': -100,
+                    'end_long': 3,
+                    'rider_name': "rider1",
+                    'driver_name': "driver1",
+                    'driver_vehicle': 'vehicle1'
+                })
+                // .end()
+                .expect("Content-Type", /json/)
+                .expect({
+                    error_code: 'VALIDATION_ERROR',
+                    message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+                }, done)
+        })
+
+        it("should return VALIDATION_ERROR", (done) => {
+            request(app)
+                .post("/rides")
+                .send({
+                    'start_lat': 1,
+                    'start_long': 2,
+                    'end_lat': 3,
+                    'end_long': '4',
+                    'rider_name': "",
+                    'driver_name': '6',
+                    'driver_vehicle': '7'
+                })
+                // .end()
+                .expect("Content-Type", /json/)
+                .expect({
+                    error_code: 'VALIDATION_ERROR',
+                    message: 'Rider name must be a non empty string'
+                }, done)
+        })
+
+        it("should return VALIDATION_ERROR", (done) => {
+            request(app)
+                .post("/rides")
+                .send({
+                    'start_lat': 1,
+                    'start_long': '2',
+                    'end_lat': 0,
+                    'end_long': '4',
+                    'rider_name': "rider1",
+                    'driver_name': "",
+                    'driver_vehicle': '7'
+                })
+                // .end()
+                .expect("Content-Type", /json/)
+                .expect({
+                    error_code: 'VALIDATION_ERROR',
+                    message: 'Driver name must be a non empty string'
+                }, done)
+        })
+
+        it("should return VALIDATION_ERROR", (done) => {
+            request(app)
+                .post("/rides")
+                .send({
+                    'start_lat': 1,
+                    'start_long': '2',
+                    'end_lat': 0,
+                    'end_long': '4',
+                    'rider_name': "rider1",
+                    'driver_name': "Driver1",
+                    'driver_vehicle': ''
+                })
+                // .end()
+                .expect("Content-Type", /json/)
+                .expect({
+                    error_code: 'VALIDATION_ERROR',
+                    message: 'Driver Vehicle must be a non empty string'
+                }, done)
+        })
+    })
+
+
+    describe('GET /rides', () => {
+        it('should return ride info on page', (done) => {
+            request(app)
+                .get('/rides')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+
+    describe('GET /rides/:rideValue', () => {
+        it('should return single ride info', (done) => {
+            request(app)
+                .get('/rides/1')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+    
 });
+
+
+
