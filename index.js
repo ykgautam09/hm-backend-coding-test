7ecb84cad2a693466bb84692501ad1b4b02cd138
ykgautam09 <yateeshgautam1357@gmail.com>
2022-01-03 01:32:58 +0530

    update project configuration

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = 8010;
 
+const logger = require('./src/logger')
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
@@ -17,5 +18,5 @@ db.serialize(() => {
 
     const app = require('./src/app')(db);
 
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+    app.listen(port, () => logger.verbose(`App started and listening on port ${port}`));
 });
